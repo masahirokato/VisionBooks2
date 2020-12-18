b1521210b1f7305b53b66b7cc233b05076ab0925
masahirokato <hcooh18@gmail.com>
2020-12-19 02:51:28 +0900

    1week session

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,2 @@
-app_name = "hoge" # ここは適当に変えるなり以前のクッキーを参照するなりしてください
-Rails.application.config.session_store :cookie_store, key: "_#{app_name}_session", expire_after: 1.day
+app_name = "Vision Equipment" # ここは適当に変えるなり以前のクッキーを参照するなりしてください
+Rails.application.config.session_store :cookie_store, key: "_#{app_name}_session", expire_after: 1.week
